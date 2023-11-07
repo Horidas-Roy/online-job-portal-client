45d04f6b3120599c5c9993a947236465da5d5ccc
Horidas Roy <royhoridas64@gmail.com>
2023-11-07 23:27:08 +0600

    applid functionality on mybid page

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const JobDetails = () => {
+  const { user } = useContext(AuthContext);
+  console.log(user?.email);
   const job = useLoaderData();
   const {
     job_title,
@@ -9,11 +12,39 @@ const JobDetails = () => {
     maximum_price,
     minimum_price,
     description,
+    employer,
     icon,
   } = job;
   console.log(job);
+
+  const handleProjectBit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const employer = form.employer.value;
+    const applicant = form.applicant.value;
+    const deadline = form.deadline.value;
+    const price = form.price.value;
+    const status='pending'
+    const user={
+      employer,applicant,deadline,price,status
+    }
+    console.log(user);
+    fetch('http://localhost:5000/bids',{
+      method:'POST',
+      headers:{
+        'content-type':'application/json'
+      },
+      body:JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data)
+    })
+
+  };
+
   return (
-    <div className="bg-base-200">
+    <form onSubmit={handleProjectBit} className="bg-base-200">
       <div className="hero bg-base-200">
         <div className="hero-content flex-col  lg:flex-row">
           <div className="w-1/4 flex justify-center items-center">
@@ -39,6 +70,8 @@ const JobDetails = () => {
               </label>
               <input
                 type="email"
+                name="applicant"
+                defaultValue={user?.email}
                 placeholder="Your Email"
                 readOnly
                 className="input input-bordered w-full max-w-xs"
@@ -50,6 +83,8 @@ const JobDetails = () => {
               </label>
               <input
                 type="text"
+                name="employer"
+                defaultValue={employer}
                 placeholder="Buyer Email"
                 readOnly
                 className="input input-bordered w-full max-w-xs"
@@ -63,6 +98,7 @@ const JobDetails = () => {
               </label>
               <input
                 type="date"
+                name="deadline"
                 placeholder="Type here deadline"
                 className="input input-bordered w-full max-w-xs"
               />
@@ -71,21 +107,27 @@ const JobDetails = () => {
               <label className="label">
                 <span className="label-text">Price $</span>
               </label>
-              <input 
-              type="text"
-              placeholder={'$'+maximum_price+'~$'+minimum_price}
-              className="input input-bordered w-full max-w-xs"
-               />
+              <input
+                type="text"
+                name="price"
+                placeholder={"$" + maximum_price + "~$" + minimum_price}
+                className="input input-bordered w-full max-w-xs"
+              />
             </div>
           </div>
           <div className="flex justify-center gap-5 my-5">
-            <button className="bg-[#007456] hover:bg-[#2c8e74] text-white font-semibold w-[52vw] py-2 rounded-lg">
-              Bid ON THE PROJECT
-            </button>
+            <input
+              type="submit"
+              disabled={user?.email === employer}
+              value="Bid ON THE PROJECT"
+              className={`bg-[#007456] hover:bg-[#2c8e74] ${
+                user?.email === employer && "bg-[#73d2b8] hover:bg-[#73d2b8]"
+              }  text-white font-semibold w-[52vw] py-2 rounded-lg`}
+            ></input>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
